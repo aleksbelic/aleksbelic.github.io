46bf5783e809430e779b1444b042eef5b03cff4e
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2022-08-04 01:09:00 +0200

    Removing unused fonts, styling, adding contact links,

---
+++ assets/js/ready.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // lil fucker
+  let consoleNoseyStyle1 =
+    'background-color: black; color: white; font-weight: bold; padding: 5px; border-radius: 5px;';
+  let consoleNoseyStyle2 =
+    'color: crimson; font-size: 22px; font-weight: bold;';
+  console.log("%c...nosey little fucker, aren't you?", consoleNoseyStyle1);
+  console.log('%c( ಠ.ಠ)', consoleNoseyStyle2);
+});
