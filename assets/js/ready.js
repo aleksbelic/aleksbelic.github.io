a5a90c7a9ecaaaeec88d259f60062bab321a1911
Aleksandar Belic Aleksanchez <aleks.belic@gmail.com>
2022-08-05 03:12:08 +0200

    Update Bootstrap to 5.2.0; new content; navbar shrink fix; highlight observer.

--- assets/js/ready.js
+++ assets/js/ready.js
@@ -1,4 +1,22 @@
+// ready
 document.addEventListener('DOMContentLoaded', () => {
+  // highlight observer
+  const highlightObserver = new IntersectionObserver(observerCallback);
+  const highlightedElemements = document.querySelectorAll(
+    '.highlight-pink, .highlight-dark'
+  );
+  highlightedElemements.forEach((element) => {
+    highlightObserver.observe(element);
+  });
+  function observerCallback(elements, observer) {
+    elements.forEach((element) => {
+      if (element.isIntersecting) {
+        element.target.classList.add('visible');
+        observer.unobserve(element.target);
+      }
+    });
+  }
+
   // lil fucker
   let consoleNoseyStyle1 =
     'background-color: black; color: white; font-weight: bold; padding: 5px; border-radius: 5px;';
--- assets/js/shrinkNavbar.js
+++ assets/js/shrinkNavbar.js
@@ -1,6 +1,5 @@
 const animatedNavbar = (() => {
-
-  let navbarElem = document.querySelector('.navbar.fixed-top');
+  let navbarElem = document.querySelector('#navbar');
   let didScroll = false;
   let shrinkNavbarOnPx = 150;
 
@@ -8,21 +7,22 @@ const animatedNavbar = (() => {
     window.addEventListener('scroll', (e) => {
       if (!didScroll) {
         didScroll = true;
-        setTimeout(scrollPage, 100);
+        setTimeout(checkShrink, 100);
       }
     });
   }
 
-  function scrollPage() {
+  function checkShrink() {
     if (window.scrollY >= shrinkNavbarOnPx) {
       navbarElem.classList.add('navbar-shrink');
-    }
-    else {
+    } else {
       navbarElem.classList.remove('navbar-shrink');
     }
     didScroll = false;
   }
 
-  init();
-
+  document.addEventListener('DOMContentLoaded', () => {
+    checkShrink();
+    init();
+  });
 })();
